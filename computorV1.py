056e3254b5d79fd7b399d93ed741b370b0eed441
seruiz <s.ruiz.sin@gmail.com>
2022-07-13 13:35:59 +0200

    remove useless stuff

--- computorV1.py
+++ computorV1.py
@@ -3,26 +3,6 @@ import numpy as np
 import math as m
 import sys
 
-class Factors:
-    class Left: 
-        x0_ = 0
-        x1 = 0
-        x2 = 0
-    class Right:
-        x0 = 0
-        x1 = 0
-        x2 = 0
-    
-def get_factors(s, f):
-    i = 0
-    while (i < len(s)):
-        if ('X' in s[i]):
-            print(s[i][2:])
-            if (s[i][2:] > '2'):
-                print("Degree Too high")
-            print(s[i])
-        i += 1
-
 def get_factors_dic(s, dic):
     i = 0
     neg = 1
@@ -43,23 +23,11 @@ def get_factors_dic(s, dic):
 
 def main():
     dic = {}
-    f = Factors
     string = ' '.join(sys.argv[1:])
     print(string)
-    tab = string.split('=')
-    left = tab[0]
-    right = tab[1]
-    left = left.split(' ')[:-1]
-    right = right.split(' ')[1:]
-    print(left)
-    print(right)
-
     s = string.split(' ')
 
     get_factors_dic(s, dic)
-
-    # get_factors(left, f.Left)
-    # get_factors(right, f.Left)
 
 
 main ()
