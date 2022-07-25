6b336ef69b44115df78ee887e0bbd5ff64c46c74
seruiz <s.ruiz.sin@gmail.com>
2022-07-25 13:16:45 +0200

    Parsing fix

--- computorV1.py
+++ computorV1.py
@@ -13,13 +13,30 @@ def get_factors_dic(s, dic):
         if ('X' in s[i]):
             if (i - 3 >= 0 and s[i - 3] == '-'):
                 neg = -1
-            if (s[i] in dic):
-                dic[s[i]] = float(s[i - 2]) * neg * reverse + float(dic[s[i]])
+            if (int(s[i][2:]) in dic):
+                dic[int(s[i][2:])] = float(s[i - 2]) * neg * reverse + float(dic[int(s[i][2:])])
             else:
-                dic[s[i]] = float(s[i - 2]) * neg * reverse
-            print(dic[s[i]])
+                dic[int(s[i][2:])] = float(s[i - 2]) * neg * reverse
             neg = 1
         i += 1
+
+def show_form(dic):
+    # dic = sorted(dic)
+    # for key in dic:
+    #     if (float(key) > 2 and dic[key] != 0):
+    #         print("Degree is too high")
+    #     print(str(key) + " " + str(dic[key]))
+
+    sorted_dic = sorted(dic)
+    for key in sorted_dic:
+        print("Key : " + str(key) + "   Value : " + str(dic[key]))
+        print ("Polynomial degree: " + str(key))
+        if (key > 2 and dic[key] != 0):
+            print("The polynomial degree is strictly greater than 2, I can't solve.")
+
+    # for key in sorted(dic):
+    #     print("Key : " + str(key) + "   Value : " + str(dic[key]))
+
 
 def main():
     dic = {}
@@ -29,5 +46,7 @@ def main():
 
     get_factors_dic(s, dic)
 
+    show_form(dic)
+    
 
 main ()
